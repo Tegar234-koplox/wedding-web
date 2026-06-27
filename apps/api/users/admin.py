38790d35039239117bb85432864926256b506cb9
Tegar234-koplox <97289906+Tegar234-koplox@users.noreply.github.com>
2026-06-28 03:13:17 +0700

    Fix CI/Backend (#36)

--- apps/api/users/admin.py
+++ apps/api/users/admin.py
@@ -6,11 +6,7 @@ from users.models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ("Wedding access", {"fields": ("role",)}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Wedding access", {"fields": ("email", "role")}),
-    )
+    fieldsets = UserAdmin.fieldsets + (("Wedding access", {"fields": ("role",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (("Wedding access", {"fields": ("email", "role")}),)
     list_display = UserAdmin.list_display + ("email", "role")
     list_filter = UserAdmin.list_filter + ("role",)
