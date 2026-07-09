e4ab72e0cbd23d7d78b03d539f68743f6aa82f7f
Tegar234-kplox <tegarsangputra2@gmail.com>
2026-07-09 19:30:11 +0700

    Weather Couture Icon Fix

---
+++ apps/web/src/types/lottie-web.d.ts
@@ -0,0 +1,19 @@
+declare module "lottie-web" {
+  type AnimationConfig = {
+    autoplay?: boolean;
+    container: Element;
+    loop?: boolean;
+    path: string;
+    renderer?: "svg" | "canvas" | "html";
+  };
+
+  type AnimationItem = {
+    destroy: () => void;
+  };
+
+  const lottie: {
+    loadAnimation: (config: AnimationConfig) => AnimationItem;
+  };
+
+  export default lottie;
+}
